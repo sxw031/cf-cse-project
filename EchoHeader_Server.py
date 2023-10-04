d32bad194bd621696ce987ade214df140f37c52c
James Wang <james@supabase.io>
2023-10-04 19:55:47 +0800

    Add files via upload

--- EchoHeader_Server.py
+++ EchoHeader_Server.py
@@ -1,24 +1,36 @@
 import os
-import socket
 import requests
-import json
+# import logging
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        self.write_response(b'')
+        # logging.error(self.headers)
     
+    def do_POST(self):
+        content_length = int(self.headers.get('content-legth',0))
+        body = self.rfile.read(content_length)
+        
+        self.write_response(body)
+        
+    def write_response(self, content):
+        
         # First, send a 200 OK response.
         self.send_response(200)
-
-        # Then send headers.
-        self.send_header('Content-type', 'text/html; charset=utf-8')
+        
+        
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        for h in self.headers:
+            self.send_header(h, self.headers[h])
         self.end_headers()
+       
+        self.wfile.write(content)
         
         # Say Hello
         self.wfile.write("Hello, HTTP!\n".encode())
         
-        # Read and print all request headers
-        self.wfile.write(self.headers)
+        print(self.headers)
             
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
