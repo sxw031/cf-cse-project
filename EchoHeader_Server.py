976994806133c96cae494f6cfb9f31417c9ff050
Shijie Wang <sxw031@sxw031.local>
2023-10-04 16:53:16 +0800

    improve and make correction for the web server to return request headers

--- EchoHeader_Server.py
+++ EchoHeader_Server.py
@@ -1,6 +1,7 @@
 import os
 import socket
 import requests
+import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class MyRequestHandler(BaseHTTPRequestHandler):
@@ -10,19 +11,25 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
 
         # Then send headers.
-        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
         
-        # Add hello otherwise no content to show
+        # Say Hello
         self.wfile.write("Hello, HTTP!\n".encode())
         
         # Read and print all request headers
-        for line in self.rfile:
-            print(line, end='')
+        self.wfile.write(self.headers)
             
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)  # Serve on all addresses, port 8000.
     httpd = HTTPServer(server_address, MyRequestHandler)
-    httpd.serve_forever()
+    
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    
+    httpd.server_close()
+    print("Server stopped")
            
